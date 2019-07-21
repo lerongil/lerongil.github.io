92be7f2c613feedff2dbcdcf932cc62769b242f4
Leron Gil <lerongil@Lerons-MacBook-Pro.local>
2019-07-20 19:35:33 -0700

    Update files

--- sketch.js
+++ sketch.js
@@ -1,67 +1,74 @@
-let x,y;
+let x, y;
+
+let d;
 
 let xspeed, yspeed;
 
-let cornerPredict;
+let r, g, b;
 
-let r,g,b;
+let w = dvd.width; 
 
-function preload() {
-	dvd = loadImage("dvd-cases-4.gif");
-	
+let h = dvd.height;
+
+let W = windowWidth;
+
+let H = windowHeight;
+
+
+
+function preload () {
+  dvd = loadImage('dvd-cases-4.gif')
 }
 
-function windowResized() {
+function windowResized () {
   resizeCanvas(windowWidth, windowHeight)
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-	x = random(width); 
-	y = random(height);
-	xspeed = 10;
-	yspeed = 10;
-  r = random(256);
-  g = random(256);
-  b = random(256);
-  pickColor();
+function setup () {
+  createCanvas(windowWidth, windowHeight)
+  x = random(width)
+  y = random(height)
+  xspeed = 10
+  yspeed = 10
+  r = random(256)
+  g = random(256)
+  b = random(256)
+  pickColor()
 }
 
-function pickColor() {
-  r = random(100, 256);
-  g = random(100, 256);
+function pickColor () {
+  r = random(100, 256)
+  g = random(100, 256)
   b = random(100, 256)
 }
-  
 
-function draw() {
-  background(0);
-	// Draw the DVD logo
-  tint(r,g,b);
-	image(dvd, x, y);
-	
-	x = x + xspeed;
-	y = y + yspeed;
-	
-	if (x + dvd.width >= width) {
-		xspeed = -xspeed;
-		x = width - dvd.width;
-    pickColor();
-	} else if (x <= 0) {
-		xspeed = -xspeed;
-		x = 0;
-    pickColor();
-	}
-	
-	
-	if (y + dvd.height >= height) {
-		yspeed = -yspeed;
-		y = height - dvd.height;
-    pickColor();
-	} else if (y <= 0) {
-		yspeed = -yspeed;
-		y = 0;
-    pickColor();
-	}
-	
+function draw () {
+  background(0)
+  // Draw the DVD logo
+  tint(r, g, b)
+  image(dvd, x, y)
+
+  x = x + xspeed
+  y = y + yspeed
+
+  if (x + dvd.width >= width) {
+    xspeed = -xspeed
+    x = width - dvd.width
+    pickColor()
+  } else if (x <= 0) {
+    xspeed = -xspeed
+    x = 0
+    pickColor()
+  }
+
+  if (y + dvd.height >= height) {
+    yspeed = -yspeed
+    y = height - dvd.height
+    pickColor()
+  } else if (y <= 0) {
+    yspeed = -yspeed
+    y = 0
+    pickColor()
+  }
+
 }
